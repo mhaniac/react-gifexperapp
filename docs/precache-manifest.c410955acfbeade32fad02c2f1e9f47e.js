f23f04f648bca9e300849d5d160834c0c499e9b7
Angel Castillo <portillocastilloa@gmail.com>
2020-10-04 20:39:43 -0600

    Docs agregado

---
+++ docs/precache-manifest.c410955acfbeade32fad02c2f1e9f47e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14e4f5893b08c541bf4b6bb8c773d00f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88ff8f34274de6078156",
+    "url": "/static/css/main.08b76a72.chunk.css"
+  },
+  {
+    "revision": "100caeaeffd01e7cd693",
+    "url": "/static/js/2.78557d1b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.78557d1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88ff8f34274de6078156",
+    "url": "/static/js/main.96a5cb1a.chunk.js"
+  },
+  {
+    "revision": "e5ee6826b74226f47af5",
+    "url": "/static/js/runtime-main.b577d66e.js"
+  }
+]);
